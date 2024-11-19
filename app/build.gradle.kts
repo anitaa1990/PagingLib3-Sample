168398d8bda743955a64599f1b1aef7a7fca29f5
Anitaa Murthy <murthyanitaa@gmail.com>
2024-11-19 15:06:18 +0530

    Updated app dependencies to the latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.an.paginglib3_sample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.an.paginglib3_sample"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
